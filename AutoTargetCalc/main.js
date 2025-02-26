52919d18529b94c8b7d7961b632a76a5988304e5
程式浪人 <raymondma529@foxmail.com>
2025-02-27 06:38:27 +0800

    codes

--- AutoTargetCalc/main.js
+++ AutoTargetCalc/main.js
@@ -18,7 +18,7 @@ const APRIL_TAG = {
 }
 
 const N = -.51; // 垂直偏移量
-const OFFSET = 0.12; // 沿边偏移量, >0 right, < 0 left
+const OFFSET = 0.164338; // 沿边偏移量, >0 right, < 0 left
 
 function calcOffsetPoint(midPoint, angle, n, offset) {
         const angleRad = (angle * Math.PI) / 180; // 将角度转换为弧度
